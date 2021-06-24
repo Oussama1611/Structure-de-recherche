951e18b57bdd6cd6d3b48ba48f97582afaecdf7d
Zarkaoui Idriss <73894181+ZarkaouiI@users.noreply.github.com>
2021-06-24 01:44:35 +0100

    Update Laboratoires.js

--- Client/src/components/Pages/Laboratoires.js
+++ Client/src/components/Pages/Laboratoires.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
-import "../../App.css";
 
+import "../../App.css";
 
 function Laboratoires() {
   const [listOfLabos, setListOfLabos] = useState([]);
   const Ids = [];
-  
+
   useEffect(() => {
     axios.get("http://localhost:3001/labos").then((response) => {
       setListOfLabos(response.data);
@@ -16,33 +16,35 @@ function Laboratoires() {
   let history = useHistory();
 
   // Extraire les Ids des respo
-  function GetId(username,index) {
-    axios.get(`http://localhost:3001/auth/getId/${username}`)
-    .then((response) => {
-      Ids[index]=response.data.id;
-    });
+  function GetId(username, index) {
+    axios
+      .get(`http://localhost:3001/auth/getId/${username}`)
+      .then((response) => {
+        Ids[index] = response.data.id;
+      });
   }
   function push(j) {
     history.push(`/profil/${Ids[j]}`);
-   };
+  }
 
   return (
     <div>
-      {listOfLabos.map((value, key) => {    
-        GetId(value.username,key);
+      {listOfLabos.map((value, key) => {
+        GetId(value.username, key);
         return (
           <div className="labo">
-            <div className="post-title">
+            <div className="labo-title">
               <Link to={`/laboratoires/${value.id}`}>
-              <a className="labo-name" >{value.laboratoire}</a>
-              </Link>  
-              Responsable:
-              <button className="post-author" onClick={() => push(key)}>{value.full_name}</button> 
-              <div className="post-body">
-                {value.summary}
-              </div>
+                <a className="labo-name">{value.laboratoire}</a>
+              </Link>
             </div>
-            
+            <div className="labo-body">{value.summary}</div>
+            <div className="labo-responsable">
+              Responsable:
+              <button className="labo-author" onClick={() => push(key)}>
+                {value.full_name}
+              </button>
+            </div>
           </div>
         );
       })}
