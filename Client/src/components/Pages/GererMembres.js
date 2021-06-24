1b3d8b7ccbb0fb8cdca719841407fc2c45fbaa4c
Zarkaoui Idriss <73894181+ZarkaouiI@users.noreply.github.com>
2021-06-24 01:39:20 +0100

    Update GererMembres.js

--- Client/src/components/Pages/GererMembres.js
+++ Client/src/components/Pages/GererMembres.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { useHistory, Link, useParams } from "react-router-dom";
 import "../../App.css";
 
-
 function GererMembres() {
   const { idTeam } = useParams();
   const [listOfMembers, setListOfMembers] = useState([]);
@@ -11,39 +10,49 @@ function GererMembres() {
 
   useEffect(() => {
     axios.get(`http://localhost:3001/auth/${idTeam}`).then((response) => {
-        setListOfMembers(response.data);
+      setListOfMembers(response.data);
     });
   }, []);
-  
-   //------------------------------- supprimer un membre
-    let history =useHistory();
-    const supprimerUser=(UserId) => {
-      axios
-      .delete(`http://localhost:3001/auth/${UserId}`, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
-      })
-      history.push(`/GererMembres/${idTeam}`);
-    }
+
+  //------------------------------- supprimer un membre
+  let history = useHistory();
+  const supprimerUser = (UserId) => {
+    axios.delete(`http://localhost:3001/auth/${UserId}`, {
+      headers: { accessToken: localStorage.getItem("accessToken") },
+    });
+    history.push(`/GererMembres/${idTeam}`);
+  };
   return (
     <div>
-    <h2 style={{textAlign:"center"}}>Les membres de mon equipe sont :</h2>
-      {listOfMembers.map((value, key) => {    
-        return (
-            <> 
-          
-                <h3 style={{textAlign:"center"}}>
+      <div className="labo-by-id">
+        <div className="login-form__header">
+          Les membres de mon équipe sont:
+        </div>
+        {listOfMembers.map((value, key) => {
+          return (
+            <>
+              <div className="labo-body">
                 <Link to={`/profil/${value.id}`}>{value.full_name}</Link>
-                </h3>
-           
-            <button onClick={() => {supprimerUser(value.id);} } className="new-post" type="submit">
-               Supprimer le membre
-            </button>
+              </div>
+              <div>
+                <button
+                  onClick={() => {
+                    supprimerUser(value.id);
+                  }}
+                  className="login-form__button"
+                  type="submit"
+                >
+                  Supprimer le membre
+                </button>
+              </div>
             </>
-        )})}
-        <div style={{marginTop:"20px"}}>
-          <Link to={`/NewMembre/${idTeam}`}>
-              <button className="new-post">Ajouter un Membre</button>
-          </Link>
+          );
+        })}
+      </div>
+      <div>
+        <Link to={`/NewMembre/${idTeam}`}>
+          <button className="new-post">Ajouter un Membre</button>
+        </Link>
       </div>
     </div>
   );
