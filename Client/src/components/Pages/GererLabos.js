e8b8c78438fd8926cba1fe535570b9e3c50dd3f9
Zarkaoui Idriss <73894181+ZarkaouiI@users.noreply.github.com>
2021-06-24 01:38:23 +0100

    Update GererLabos.js

--- Client/src/components/Pages/GererLabos.js
+++ Client/src/components/Pages/GererLabos.js
@@ -3,69 +3,76 @@ import React, { useEffect, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import "../../App.css";
 
-
 function GererLabos() {
   const [listOfLabos, setListOfLabos] = useState([]);
   const Ids = [];
-  
+
   //------------------------------
   useEffect(() => {
     axios.get("http://localhost:3001/labos").then((response) => {
       setListOfLabos(response.data);
     });
   }, []);
-  
-  
+
   let history = useHistory();
 
   // extraction des Id des respo
-  function GetId(username,index) {
-    axios.get(`http://localhost:3001/auth/getId/${username}`)
-    .then((response) => {
-      Ids[index]=response.data.id;
-    });
-   }
-  
+  function GetId(username, index) {
+    axios
+      .get(`http://localhost:3001/auth/getId/${username}`)
+      .then((response) => {
+        Ids[index] = response.data.id;
+      });
+  }
+
   function push(j) {
     history.push(`/profil/${Ids[j]}`);
-   };
-   //------------------------------- supprimer un labo
+  }
+  //------------------------------- supprimer un labo
 
-    const supprimerLabo=(laboId) => {
-      axios
-      .delete(`http://localhost:3001/labos/${laboId}`, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
-      })
-      history.push("/profil/1");
-  
-    }
+  const supprimerLabo = (laboId) => {
+    axios.delete(`http://localhost:3001/labos/${laboId}`, {
+      headers: { accessToken: localStorage.getItem("accessToken") },
+    });
+    history.push("/profil/1");
+  };
 
   return (
     <div>
-      {listOfLabos.map((value, key) => {    
-        GetId(value.username,key);
+      {listOfLabos.map((value, key) => {
+        GetId(value.username, key);
         return (
           <div className="labo">
-            <div className="post-title">
+            <div className="labo-title">
               <Link to={`/laboratoires/${value.id}`}>
-              <a className="labo-name" >{value.laboratoire}</a>
-              </Link>  <br></br>
-              <p style={{color:"purple"}}>Responsable:</p>
-              <button className="post-author" onClick={() => push(key)}>{value.full_name}</button> 
-              <div className="post-body">
-                {value.summary}
-              </div>
+                <a className="labo-name">{value.laboratoire}</a>
+              </Link>{" "}
+              <br />
             </div>
-            <button onClick={() => {supprimerLabo(value.id);} } className="login-form__button" type="submit">
-               Supprimer le laboratoire
+            <div className="labo-responsable">
+              Responsable:
+              <button className="labo-author" onClick={() => push(key)}>
+                {value.full_name}
+              </button>
+            </div>
+
+            <div className="labo-body">{value.summary}</div>
+            <button
+              onClick={() => {
+                supprimerLabo(value.id);
+              }}
+              className="login-form__button"
+              type="submit"
+            >
+              Supprimer le laboratoire
             </button>
           </div>
         );
       })}
-      <div>
-          <Link to="/NewLabo">
-              <button className="new-post">Ajouter un Laboratoire</button>
-          </Link>
+      <div className="login-container">
+        <Link to="/NewLabo">
+          <button className="login-form__button">Ajouter un Laboratoire</button>
+        </Link>
       </div>
     </div>
   );
